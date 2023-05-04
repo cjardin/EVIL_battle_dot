8a6a73f9d1cdcbdc46ebd650b6d518f7edb6f008
Abiru22 <131930502+Abiru22@users.noreply.github.com>
2023-05-04 13:06:14 -0700

    my battlebots

--- behaviors/jasonX.py
+++ behaviors/jasonX.py
@@ -4,6 +4,4 @@ def update(dna, d_id, state,  db_cursor):
         #get all my dots
         rows = db_cursor.execute(f"select x,y from main_game_field where is_flag = FALSE and  d_id = '{d_id}'")
         for row in rows.fetchall():
-            db_cursor.execute(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0] + random.choice([0,1,-1]) }, {row[1] + random.choice([0,1,-1]) }, 'MOVE', '{d_id}')")
-
-
+            db_cursor.execute(f"insert into engine_orders values( {row[0]}, {row[2]}, {row[0] + random.choice([0,1,-2]) }, {row[2] + random.choice([0,1,-2]) }, 'MOVE', '{d_id}')")
