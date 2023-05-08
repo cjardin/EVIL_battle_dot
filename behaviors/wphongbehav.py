32b402d992ecf019d10c0ea3baaead787d779302
William Phong <williamphong@users.noreply.github.com>
2023-05-07 21:33:26 -0700

    update

--- behaviors/wphongbehav.py
+++ behaviors/wphongbehav.py
@@ -7,7 +7,8 @@ def update(dna, d_id, state,  db_cursor):
         if (row[0],row[1]) not in my_dots or (random.random() > .95) :
             my_dots[ (row[0],row[1]) ] = ( random.choice([1,-1]) , random.choice([1,-1]) )
         offset = my_dots[ (row[0],row[1]) ]
-        db_cursor.execute(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0] + offset[0] }, {row[1] + offset[1] }, 'MOVE', '{d_id}')")
-        del my_dots[ (row[0],row[1]) ]
-        my_dots[ (row[0] + offset[0], row[1] + offset[1]) ] = offset
+        
+    db_cursor.execute(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0] + offset[0] }, {row[1] + offset[1] }, 'MOVE', '{d_id}')")
+    del my_dots[ (row[0],row[1]) ]
+    my_dots[ (row[0] + offset[0], row[1] + offset[1]) ] = offset
 
