39cfecb115ca435a838c8d6eac1afb5aa23a7477
kenny <kxwang2@icloud.com>
2023-05-08 15:13:55 -0700

    first update

---
+++ behaviors/pandaWalk.py
@@ -0,0 +1,13 @@
+import random
+
+def update(dna, d_id, state,  db_cursor):
+    my_dots = state
+    rows = db_cursor.execute(f"select x,y from main_game_field where is_flag = FALSE and  d_id = '{d_id}'")
+    for row in rows.fetchall():
+        if (row[0],row[1]) not in my_dots or (random.random() > .85) :
+            my_dots[ (row[0],row[1]) ] = ( random.choice([1,-1]) , random.choice([1,-1]) )
+        offset = my_dots[ (row[0],row[1]) ]
+        db_cursor.execute(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0] + offset[0] }, {row[1] + offset[1] }, 'MOVE', '{d_id}')")
+        del my_dots[ (row[0],row[1]) ]
+        my_dots[ (row[0] + offset[0], row[1] + offset[1]) ] = offset
+
