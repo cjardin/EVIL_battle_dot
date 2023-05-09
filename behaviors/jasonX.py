d24399018468e996fb425553bd623ad76a6a85d6
Abiru22 <mannyberu@gmail.com>
2023-05-08 18:48:41 -0700

    chang3
    another change

--- behaviors/jasonX.py
+++ behaviors/jasonX.py
@@ -5,5 +5,7 @@ def update(dna, d_id, state,  db_cursor):
         rows = db_cursor.execute(f"select x,y from main_game_field where is_flag = FALSE and  d_id = '{d_id}'")
         for row in rows.fetchall():
             db_cursor.execute(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0] + random.choice([0,1,-1]) }, {row[1] + random.choice([0,1,-1]) }, 'MOVE', '{d_id}')")
-
-
+            
+            
+            
+           
