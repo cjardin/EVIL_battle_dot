63a7b2ea296d6a46932b4fb09d3ca103b13f0c53
Adam-Lazelle <adam.lazelle@gmail.com>
2023-05-08 19:35:31 -0700

    Dumbed down the behavior
    Dumbed down the behavior so there is no longer a compile issue

--- behaviors/budder.py
+++ behaviors/budder.py
@@ -1,26 +1,12 @@
 import random
-import math
-   #this is a behavior for budder
-def update(dna, d_id, state,  db_cursor):
-       my_dots = state
-   
-    if("no_food" not in state):
-    state.update({"no_food": False})                                                                                                                                                  
-       
-    if(state["no_food"]):
-              
-    for row in rows.fetchall():
 
-    if (row[0],row[1]) not in my_dots or (random.random() > .99) :
-    my_dots[ (row[0],row[1]) ] = ( random.choice([1,-1]) , random.choice([1,-1]) ) offset = my_dots[ (row[0],row[1]) ]
-              db_cursor.execute(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0] + offset[0] }, {row[1] + offset[1] }, 'MOVE', '{d_id}')")
-              del my_dots[ (row[0],row[1]) ]
-              my_dots[ (row[0] + offset[0], row[1] + offset[1]) ] = offset
-              
-        else:
-              trees = db_cursor.execute(f"select x,y from main_game_field where is_flag = FALSE and owner_id = '0'")
-            trees.fetchall()
-              if(not trees):
-                state.update({"no_food": True})
-                  return
-                  
+def update(dna, d_id, state,  db_cursor):
+    my_dots = state
+    rows = db_cursor.execute(f"select x,y from main_game_field where is_flag = FALSE and  d_id = '{d_id}'")
+    for row in rows.fetchall():
+        if (row[0],row[1]) not in my_dots or (random.random() > .99) :
+            my_dots[ (row[0],row[1]) ] = ( random.choice([1,-1]) , random.choice([1,-1]) )
+        offset = my_dots[ (row[0],row[1]) ]
+        db_cursor.execute(f"insert into engine_orders values( {row[0]}, {row[1]}, {row[0] + offset[0] }, {row[1] + offset[1] }, 'MOVE', '{d_id}')")
+        del my_dots[ (row[0],row[1]) ]
+        my_dots[ (row[0] + offset[0], row[1] + offset[1]) ] = offset
